02d9e821aa5b54c96fb6834cabdaee320266d27a
Your Name <you@example.com>
2024-01-02 11:32:35 +0200

    add terms and return policy 2/1/2023

--- us_school/plugins/sweet-alert.ts
+++
@@ -1,8 +0,0 @@
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
-
-
-
-export default defineNuxtPlugin(({ vueApp }) => {
-    vueApp.use(VueSweetalert2)
-})
